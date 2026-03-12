d476d74c995ec50c380b29203752f3d8c71083cd
Steavens <steavens2010@aol.com>
2026-03-12 13:03:11 +0400

    v1.2 - ajout d un troisieme affichage

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Version 1.1");
+        System.out.println("Version 1.2");
     }
 }

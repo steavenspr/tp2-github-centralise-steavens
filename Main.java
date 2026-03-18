4e8128471e9d8ea14192b155dfd5cfb72fab7c55
Steavens <steavens2010@aol.com>
2026-03-18 15:48:33 +0400

    Modification Steavens v3

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Version 1.1");
         System.out.println("Version 1.2");
         System.out.println("Travail sur branche feature");
-        System.out.println("Ceci est la version de Steve et Steavens v2");
+        System.out.println("Ceci est la version de Steve et Steavens v3");
     }
 }
